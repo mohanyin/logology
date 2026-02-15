a66099a7ccce487a7d7ea9ee2d61bb8fbbb09b11
Mohan Yin <mohanyin@gmail.com>
2026-02-15 18:05:32 -0500

    add shader

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { NeuroNoise } from "@paper-design/shaders-react";
 import Grid from "@/components/board/Grid";
 
 function App() {
   return (
-    <div className="bg-neutral-light flex min-h-screen items-center justify-center">
+    <div className="relative flex min-h-screen items-center justify-center">
+      <div className="fixed inset-0 -z-10">
+        <NeuroNoise
+          width="100%"
+          height="100%"
+          colorFront="#fffef4"
+          colorMid="#f0eee1"
+          colorBack="#e1decb"
+          brightness={0.05}
+          contrast={0.3}
+          speed={0.5}
+        />
+      </div>
       <Grid />
     </div>
   );
